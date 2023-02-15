36a7a0f088a93aa8015df5379d07ea74ae140979
juanma785 <115530852+juanma785@users.noreply.github.com>
2023-02-15 12:13:07 +0100

    Add files via upload

---
+++ juanmanuelsaborido_ej2/exud4/JuanmanuelSaborido_Ej2ExUD4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.juanmanuelsaborido_ej2.exud4;
+
+/**
+ *
+ * @author 1DAW_2223_13
+ */
+public class JuanmanuelSaborido_Ej2ExUD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
